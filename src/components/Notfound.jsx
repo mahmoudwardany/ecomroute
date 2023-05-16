93d8e6cdc8365ea9256391ad2f4e105e352290fc
mahmoud <mahmoudwardany18@gmail.com>
2023-05-16 19:05:29 +0300

    final

--- src/components/Notfound.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Notfound() {
-  return (
-    <div>Notfound 404</div>
-  )
-}
-
-export default Notfound
--- src/share/ProtectRoute.js
+++ src/share/ProtectRoute.js
@@ -1,9 +1,8 @@
 import { Navigate,useLocation } from "react-router-dom";
 
 const ProtectRoute = ({children}) => {
-const location=useLocation()
 if(localStorage.getItem('userToken')===null){
-  return  <Navigate to='/login' state={{path:location.pathname}}/>
+  return  <Navigate to='/login' />
 }
     return children
 }
